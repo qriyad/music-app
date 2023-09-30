1c5d865135099f0995d027cf086105ecdacf1e58
Riyad <riyadqhrmanli@gmail.com>
2023-09-30 20:19:31 +0400

    main py & models py finished

---
+++ back/main.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlmodel import Field, SQLModel, Session, create_engine
+from fastapi.middleware.cors import CORSMiddleware
+from models import User, Playlist, Song, Favourite
+
+db_url = './db.sqlite'
+DATABASE_URL = f"sqlite:///{db_url}"
+
+app = FastAPI()
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SQLModel.metadata.create_all(engine)
+
+def get_db():
+    with Session(engine) as db:
+        yield db
--- back/models.py
+++ back/models.py
@@ -1,9 +1,9 @@
-from sqlmodel import Field, SQLModel, Session, create_engine, Relationship
+from sqlmodel import Field, SQLModel, Relationship
 from typing import List, Optional
 from datetime import datetime
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, index=True, foreign_key=True)
+    id: Optional[int] = Field(primary_key=True, index=True)
     name: str
     surname: str
     create_date: datetime = Field(default_factory=datetime.utcnow)
@@ -11,11 +11,11 @@ class User(SQLModel, table=True):
     password: str
     playlists: List["Playlist"] = Relationship(back_populates="user")
     songs: List["Song"] = Relationship(back_populates="user")
+    favourites: List["Favourite"] = Relationship(back_populates="user")
 
 class Playlist(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, index=True, foreign_key=True)
-    song_id: int
-    user_id: int
+    id: Optional[int] = Field(primary_key=True, index=True)
+    user_id: int = Field(foreign_key="user.id")
     title: str
     description: str
     image: str
@@ -24,15 +24,18 @@ class Playlist(SQLModel, table=True):
     songs: List["Song"] = Relationship(back_populates="playlist")
 
 class Song(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, index=True, foreign_key=True)
-    user_id: int
+    id: Optional[int] = Field(primary_key=True, index=True)
+    user_id: int = Field(foreign_key="user.id")
+    playlist_id: int = Field(foreign_key="playlist.id")
     create_date: datetime = Field(default_factory=datetime.utcnow)
     title: str
     duration: str
     user: User = Relationship(back_populates="songs")
     playlist: Playlist = Relationship(back_populates="songs")
+    favourites: List["Favourite"] = Relationship(back_populates="song")
 
 class Favourite(SQLModel, table=True):
-    user_id_song_id: Optional[int] = Field(primary_key=True, index=True, foreign_key=True)
+    user_id: int = Field(primary_key=True, index=True, foreign_key="user.id")
+    song_id: int = Field(primary_key=True, index=True, foreign_key="song.id")
     user: User = Relationship(back_populates="favourites")
     song: Song = Relationship(back_populates="favourites")
