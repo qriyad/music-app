bc53e2b79c0ac86a8260ea73db0ac13efe47b0c4
Riyad <riyadqhrmanli@gmail.com>
2023-10-01 11:17:23 +0400

    main py user crud finished

--- back/main.py
+++ back/main.py
@@ -60,3 +60,59 @@ class TokenData(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+@app.post('/users/', response_model=User, tags=['Users'])
+async def create_user(user: User, db: Session = Depends(get_db)):
+    statement = select(User).where(User.email == user.email)
+    db_user = db.exec(statement).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return 'User Created succsessfully'
+
+@app.get("/users/{user_id}", response_model=User, tags=['Users'])
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+@app.put("/users/{user_id}", response_model=User, tags=['Users'])
+async def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
+    db_user = db.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    user_data = user.dict(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+    
+    db.commit()
+    db.refresh(db_user)
+    return 'User Updated succsessfully'
+
+@app.delete("/users/{user_id}", response_model=User, tags=['Users'])
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    db.delete(db_user)
+    db.commit()
+    return 'User Removed Succsessfully'
+"""
+@app.post('/schedules/', response_model=Schedule, tags=['Schedules'])
+async def create_schedule(schedule: Schedule, db: Session = Depends(get_db)):
+    user = db.get(User, schedule.user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    note = db.get(Note, schedule.note_id)  
+    if not note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+    db_schedule = Schedule.from_orm(schedule)
+    db.add(db_schedule)
+    db.commit()
+    db.refresh(db_schedule)
+    return db_schedule
+"""
